8d0dc233d9cf5394be189a0bf2a1f93ea5a3c71a
MarlenBalderrama <L25550651@chihuahua2.tecnm.mx>
2026-09-03 16:50:47 -0600

    Add files via upload

---
+++ EVA_1_2_STACK/src/eva_1_2_stack/EVA_1_2_STACK.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_2_stack;
+
+/**
+ *
+ * @author marle
+ */
+public class EVA_1_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("inicia main");
+       A();
+        System.out.println("termina main");
+    }
+    public static void A(){
+        System.out.println("inicia A()");
+    B();
+        System.out.println("termina A()");
+    }
+    public static void B(){
+        System.out.println("inicia B()");
+    int i =5;
+    double d = 5.5;
+        System.out.println("termina A()");
+    }
+        
+        
+    
+    
+}

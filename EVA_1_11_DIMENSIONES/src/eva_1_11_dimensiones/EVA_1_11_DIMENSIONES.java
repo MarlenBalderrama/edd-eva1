b425a4443752003b5f4d3f6f7fdbf1fc76692c11
MarlenBalderrama <L25550651@chihuahua2.tecnm.mx>
2026-09-15 18:42:11 -0600

    Add files via upload

---
+++ EVA_1_11_DIMENSIONES/src/eva_1_11_dimensiones/EVA_1_11_DIMENSIONES.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_11_dimensiones;
+
+/**
+ *
+ * @author marle
+ */
+public class EVA_1_11_DIMENSIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [][] matriz = new int [3][];
+        //cada fila tendrá diferente cantidad de columnas:
+        matriz[0] = new int [5];
+        matriz[1] = new int [10];
+        matriz[2] = new int [3];
+        
+        for (int i=0; i<matriz.length; i++){
+           for(int j=0; j<matriz[i].length; j++){
+            matriz[i][j] = (int)(Math.random()*100);
+            
+           }
+        }
+        
+        for (int i=0; i< matriz.length; i++){
+           for (int j=0; j<matriz[i].length; j++){
+                System.out.println("[" + matriz[i][j] + "]");
+           }
+            System.out.println("");
+        }
+                
+    }
+    
+}
